7fb8e3d5becadb48f1feffba8b58c537d2961a35
Patrick Govus <patrickgovus99@gmail.com>
2023-07-24 19:01:51 +0100

    2nd commit

--- hangman.py
+++ hangman.py
@@ -5,10 +5,34 @@ possible_words = ['mango', 'pineapple', 'passionfruit', 'peach', 'orange']
 
 # print(random.choice(possible_words))
 
-print(f'please choose a guess')
-guess = input()
-if guess.isalpha():
-    print('good guess')
-else:
-    print('Oops! That is not a valid input.')
-print('you have guessed:', guess)
+
+word = random.choice(possible_words)
+# print(word)0
+letters = list(word)
+print(letters)
+
+
+def ask_for_input(letter):
+    # print(letter)
+    print('you have guessed:', letter)
+    if not letter.isalpha():
+        print(f'{letter} is not a letter, please enter a new letter')
+        letter = input()
+    else:    
+        letter = letter.lower()
+    return str(letter)
+
+def check_guess(guess):
+
+    if guess in letters:
+        print('good guess')
+    else:
+        print(f'{guess} is not in the word')
+        
+
+    
+
+if __name__ == '__main__':
+    print('please input a character:')
+    new_guess = ask_for_input(input())
+    check_guess(new_guess)
