9509a340ed033772de3ea3759b0f17afef624878
Patrick Govus <patrickgovus99@gmail.com>
2023-07-24 19:13:05 +0100

    3rd commit, class started, logic tomorrow

--- hangman.py
+++ hangman.py
@@ -11,28 +11,40 @@ word = random.choice(possible_words)
 letters = list(word)
 print(letters)
 
+class hangman:
+    def __init__(self, word, word_guessed, num_letters, num_lives, word_list, list_of_guesses):
+        self.word = word
+        self.word_guessed = word_guessed
+        self.num_letters = num_letters
+        self.num_lives = num_lives
+        self.word_list = word_list
+        self.list_of_guesses = list_of_guesses
+        pass
 
-def ask_for_input(letter):
-    # print(letter)
-    print('you have guessed:', letter)
-    if not letter.isalpha():
-        print(f'{letter} is not a letter, please enter a new letter')
-        letter = input()
-    else:    
-        letter = letter.lower()
-    return str(letter)
 
-def check_guess(guess):
 
-    if guess in letters:
-        print('good guess')
-    else:
-        print(f'{guess} is not in the word')
-        
+    def ask_for_input(self, letter):
+        print('you have guessed:', letter)
+        if not letter.isalpha():
+            print(f'{letter} is not a letter, please enter a new letter')
+            letter = input()
+        else:    
+            letter = letter.lower()
+        return str(letter)
+
+
+    def check_guess(self, guess):
+
+        if guess in letters:
+            print('good guess')
+        else:
+            print(f'{guess} is not in the word')
+            
 
     
 
 if __name__ == '__main__':
     print('please input a character:')
-    new_guess = ask_for_input(input())
-    check_guess(new_guess)
+    hanging_about = hangman()
+    new_guess = hanging_about.ask_for_input(input())
+    hanging_about.check_guess(new_guess)
